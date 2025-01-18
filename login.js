14130c9e3c211677c5a39c86296654cc6afc0d5c
HalfSmore <99419482+HalfSmore@users.noreply.github.com>
2025-01-18 17:18:27 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    const users = 
+    [
+        "john",
+        "jimmy",
+        "jemimah",
+        "jack",
+        "julian",
+        "brandon"
+
+    ]
+    document.getElementById("button").onclick = function showSubmitText()
+    {
+        var flag = "BCPSctf{supp0s3d_t0_b3_xxl_1nj3ct10n}";
+        var name = document.getElementById("username").value;
+        if(users.includes(name))
+        {
+            window.location.href = "https://epicgamez.w3spaces.com/gamez.html";
+        }
+        else
+        {
+            document.getElementById("submittext").innerHTML = "Sorry, there is no user " + name;
+            eval(name);
+        }
+       
+    }
+}

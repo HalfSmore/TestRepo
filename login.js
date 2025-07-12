990c67b257b9be71f0d97d6bd8eb8cfb5c5b7c53
HalfSmore <99419482+HalfSmore@users.noreply.github.com>
2025-07-12 15:56:33 -0400

    Update login.js

--- login.js
+++ login.js
@@ -11,7 +11,7 @@ window.onload = function() {
     ]
     document.getElementById("button").onclick = function showSubmitText()
     {
-        var flag = "BCPSctf{supp0s3d_t0_b3_xxl_1nj3ct10n}";
+        var flag = "fkag{supp0s3d_t0_b3_xxl_1nj3ct10n}";
         var name = document.getElementById("username").value;
         if(users.includes(name))
         {
